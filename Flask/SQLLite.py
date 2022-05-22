5766b33c1a9d528565b284a5d578c9911a1d6115
Ronny C <41557151+RonnyCh@users.noreply.github.com>
2022-05-22 19:47:12 +1000

    updated

--- Flask/SQLLite.py
+++ Flask/SQLLite.py
@@ -6,3 +6,7 @@ print ("Opened database successfully");
 conn.execute('CREATE TABLE timesheet (date TEXT, day TEXT, company TEXT, rate TEXT)')
 print ("Table created successfully");
 conn.close()
+
+
+#os.chdir(r'c:\Users\r.christianto\SQLiteStudio')
+#con = sqlite3.connect("mydatabase")
