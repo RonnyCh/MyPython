d8e75a51c3941c08961337944a7e1b5976d4d0dc
Ronny C <41557151+RonnyCh@users.noreply.github.com>
2022-05-27 11:32:40 +1000

    updated

--- FTP/ftp.py
+++ FTP/ftp.py
@@ -9,33 +9,50 @@ ftp = FTP_TLS('ccapaprod.planning-analytics.ibmcloud.com')
 ftp.login(user='FileShare', passwd='3KThy8DJpk6RWH')
 ftp.prot_p()    # you have to run this otherwise won't work
 # change dir
+#ftp.cwd("/prod/LCA/XeroImport/XeroExport/dist/api_output")
+ftp.cwd("/prod/LCA/Upload Templates")
+# list files
+ftp.dir()
+ftp.quit()
+
+filename = 'demofile2.txt'
+
+filename = 'NonFinancial Budget Upload.xlsx'
+
+########### download file from FTP Server ##############
+with open(filename, "wb") as file:
+    # Command for Downloading the file "RETR filename"
+    ftp.retrbinary(f"RETR {filename}", file.write)
+
+
+########## upload the file to FTP Server ###############
+########## still not working ######################
+ftp.set_debuglevel(2)
+ftp.encoding = "utf-8"
+with open(filename, "rb") as file:
+    ftp.prot_p()    # you have to run this otherwise won't work
+    # Command for Uploading the file "STOR filename"
+    ftp.storbinary(f"STOR {filename}", file,1024)
+
+########### delete a file ##########
+ftp.delete('demofile2.txt')
+
+
+
+
+###### trying without ftp ssl ######
+from ftplib import FTP
+from pathlib import Path
+
+ftp = FTP('ccapaprod.planning-analytics.ibmcloud.com')
+
+ftp = FTP_TLS()
+ftp.connect('ccapaprod.planning-analytics.ibmcloud.com',21)
+ftp.login(user='FileShare', passwd='3KThy8DJpk6RWH')
+ftp.prot_p()  
 ftp.cwd("/prod/LCA/Upload Templates")
 # list files
 ftp.dir()
 
-
-
-
-
-
-# Connect and login at once
-with FTP_TLS(host='ccapaprod.planning-analytics.ibmcloud.com', user='FileShare', passwd='3KThy8DJpk6RWH') as ftp:
-    #print(ftp.pwd())  # Usually default is /
-        # For text or binary file, always use `rb`
-    with open('test.txt', 'rb') as text_file:
-        ftp.storlines('STOR test.txt', text_file)
-
-
-
-
-LCA =  FTP_TLS(host='ccapaprod.planning-analytics.ibmcloud.com', user='FileShare', passwd='3KThy8DJpk6RWH') 
-
-
-filenames = LCA.nlst() # get filenames within the directory
-print filenames
-
-
-
-print(LCA.cwd('prod/LCA')) 
-
-LCA.dir()
+with FTP('ftpes://ccapaprod.planning-analytics.ibmcloud.com', 'FileShare', '3KThy8DJpk6RWH') as ftp, open(file_path, 'rb') as file:
+        ftp.storbinary(f'STOR {file_path.name}', file)
