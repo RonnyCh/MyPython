7161208cad635595d299ca754125e91bd2eecd7c
Ronny C <41557151+RonnyCh@users.noreply.github.com>
2022-05-30 09:01:09 +1000

    updated

--- Screen Record/Test Simulation/Run Simulation.bat
+++
@@ -1,7 +0,0 @@
-
-
-
-call "C:\Users\r.christianto\anaconda3\Scripts\activate.bat"
-python "C:\Users\r.christianto\MyPython\Screen Record\Test Simulation\TeamStatus.py"
-pause
-
--- Screen Record/Test Simulation/TeamStatus.py
+++
@@ -1,9 +0,0 @@
-import offline
-import away
-import time
-
-
-offline.offline()
-time.sleep(5)
-away.away()
-
