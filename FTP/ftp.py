aa2e89b7def55ca9936e582c47d4085ec5acc444
Ronny C <41557151+RonnyCh@users.noreply.github.com>
2022-06-02 16:58:00 +1000

    updated

--- FTP/ftp.py
+++ FTP/ftp.py
@@ -2,7 +2,7 @@
 
 # https://www.devdungeon.com/content/python-ftp-client-tutorial#:~:text=FTP%20or%20File%20Transfer%20Protocol%20is%20a%20common,%28SFTP%29%20that%20uses%20SSH%20to%20encrypt%20the%20communication.
 
-
+import os
 import ftplib
 from ftplib import FTP_TLS
 ftp = FTP_TLS('ccapaprod.planning-analytics.ibmcloud.com')
@@ -15,9 +15,9 @@ ftp.cwd("/prod/LCA/Upload Templates")
 ftp.dir()
 ftp.quit()
 
-filename = 'demofile2.txt'
+# chdir
+os.chdir(r'C:\Users\r.christianto\MyPython\FTP')
 
-filename = 'NonFinancial Budget Upload.xlsx'
 
 ########### download file from FTP Server ##############
 with open(filename, "wb") as file:
@@ -25,34 +25,21 @@ with open(filename, "wb") as file:
     ftp.retrbinary(f"RETR {filename}", file.write)
 
 
+
+
 ########## upload the file to FTP Server ###############
-########## still not working ######################
-ftp.set_debuglevel(2)
-ftp.encoding = "utf-8"
+########## finallly working ######################
+filename = 'NonFinancial Budget UploadTest.xlsx'
+#ftp.set_debuglevel(2)
+ftplib._SSLSocket = None   #### this is key to make upload work!!!! (from reza code)
 with open(filename, "rb") as file:
     ftp.prot_p()    # you have to run this otherwise won't work
     # Command for Uploading the file "STOR filename"
     ftp.storbinary(f"STOR {filename}", file,1024)
 
+
+
 ########### delete a file ##########
-ftp.delete('demofile2.txt')
-
-
-
-
-###### trying without ftp ssl ######
-from ftplib import FTP
-from pathlib import Path
-
-ftp = FTP('ccapaprod.planning-analytics.ibmcloud.com')
-
-ftp = FTP_TLS()
-ftp.connect('ccapaprod.planning-analytics.ibmcloud.com',21)
-ftp.login(user='FileShare', passwd='3KThy8DJpk6RWH')
-ftp.prot_p()  
-ftp.cwd("/prod/LCA/Upload Templates")
-# list files
+ftp.delete(filename)
 ftp.dir()
 
-with FTP('ftpes://ccapaprod.planning-analytics.ibmcloud.com', 'FileShare', '3KThy8DJpk6RWH') as ftp, open(file_path, 'rb') as file:
-        ftp.storbinary(f'STOR {file_path.name}', file)
