d796de435770e152523b02570d768a9e3db2c532
Ronny C <41557151+RonnyCh@users.noreply.github.com>
2022-06-02 18:54:50 +1000

    update

--- XeroAPI/Run.bat
+++ XeroAPI/Run.bat
@@ -1,4 +1,5 @@
 call "C:\Users\r.christianto\anaconda3\Scripts\activate.bat"
 python "C:\Users\r.christianto\MyPython\XeroAPI\LCAXeroAPI.py"
+python "C:\Users\r.christianto\MyPython\XeroAPI\PlayTM1API.py"
 
 pause
